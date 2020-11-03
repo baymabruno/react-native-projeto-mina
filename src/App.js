01c2ea8ca2f72fed76e8884b54f6231eb2b05b4e
Bayma Bruno <baymabruno@gmail.com>
2020-11-02 21:10:16 -0300

    70. Componente Tabuleiro (MineField)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import Field from './components/Field';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import MineField from './components/MineField';
+import { createMineBoard } from './functions';
 import params from './params';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.createState();
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmmount();
+    return Math.ceil(cols * rows * params.difficultLeve);
+  };
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmmount();
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount),
+    };
+  };
+
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Text>Iniciando o Mines!!!</Text>
         <Text>
           Tamanho da grade: {params.getColumnsAmount()} x
           {params.getRowsAmmount()}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#aaa',
+  },
+});
